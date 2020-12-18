ec1aca61232dd2b4cb75a085e77d10dd3dc9789e
Olena Vaskevych <olenaj21@gmail.com>
2020-12-18 11:35:06 +0100

    optimization

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,25 +1,35 @@
-import React from "react"
+import React, { Component } from "react"
 import Button from "../../UI/Button/Button"
 
-const order = (props) => {
+class Order extends Component {
+    //this could be a functional component
+    componentWillUpdate() {
+        console.log("updated CompWillUpdate")
+    }
 
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map((igKey) => {
-            return <li key={igKey}>
-                <span style={{ textTransform: "capitalize" }}>{igKey}:</span>{props.ingredients[igKey]}
-            </li>
-        })
-    return <>
-        <h3>Your Order</h3>
-        <p>Your Burger with delicious ingredients: </p>
-        <ul>
-            {ingredientSummary}
-        </ul>
-        <h4>Total Price: {props.price.toFixed(2)}</h4>
-        <p>Continue to checkout?</p>
-        <Button btnType="Danger" clicked={props.purchaseCancel}>Cancel</Button>
-        <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
-    </>
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map((igKey) => {
+                return <li key={igKey}>
+                    <span style={{ textTransform: "capitalize" }}>{igKey}:</span>{this.props.ingredients[igKey]}
+                </li>
+            })
+
+        return (
+            <>
+                <h3>Your Order</h3>
+                <p>Your Burger with delicious ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <h4>Total Price: {this.props.price.toFixed(2)}</h4>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancel}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>Continue</Button>
+            </>
+        )
+    }
+
 }
 
-export default order
+export default Order
